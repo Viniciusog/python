12c38391d63a63a5aecc1a1a0430d59910ccff6d
viniciusog <vinijipa23@gmail.com>
2023-02-15 12:49:42 -0300

    first

---
+++ HelloWorld/app.py
@@ -0,0 +1,15 @@
+# data_nascimento = input("data de nascimento")
+# idade = 2023 - int(data_nascimento)
+# print(idade)
+#
+# algo = input("Digite alguma coisa")
+# ok = False or bool(algo)
+# print(ok)
+#
+# idade = input("Insira a sua idade")
+# print(type(idade))
+#
+# peso = int(input("Insira o seu peso: "))
+# print(peso * 2.5)
+
+uma_string = "apenas um teste"
