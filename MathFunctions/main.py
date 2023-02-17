4456233b87ff3bad044e005b13ebd8cf9d151ea5
viniciusog <vinijipa23@gmail.com>
2023-02-17 13:42:11 -0300

    string | cast and math

---
+++ MathFunctions/main.py
@@ -0,0 +1,24 @@
+import math
+
+pi = 3.9
+# De 3.5 para cima irá arredondar para 4.
+# De 3.49 para baixo, irá arrendondar para 3
+print(round(pi)) # 3
+
+# Irá considerar o primeiro inteiro mais alto
+print(math.ceil(pi))
+
+# Irá considerar o primeiro inteiro mais baixo
+print(math.floor(pi))
+
+print(abs(pi))
+
+# 2 ^ (3)
+print(pow(2, 3))
+
+print(math.sqrt(pi))
+
+x = 5
+y = 3
+z = 6
+print(max(x, y, z))
